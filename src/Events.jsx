105c0c1561d0d1795e392549de31dc578e730098
Elias Peteri <elias.peteri@hotmail.com>
2020-12-16 11:31:59 +0200

    Feat: can now remove events

--- src/Events.jsx
+++ src/Events.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import EventService from "./EventService";
 import moment from "moment";
+
+// This component handles the form for creating new events for a specific day selected in the calendar
 const EventForm = (props) => {
   return (
     <form onSubmit={props.addEvent}>
       <div>
-        Title: <input value={props.title} onChange={props.htc} />
+        Title: <input value={props.title} onChange={props.htc} required />
       </div>
       <div>
         Description: <input value={props.event} onChange={props.hec} />
@@ -20,23 +22,30 @@ const EventForm = (props) => {
   );
 };
 
+// This component handles listing all the events for the selected date in the calendar
 const EventList = (props) => {
   return (
     <div>
       {props.currentDate}
       {props.events.map((event) =>
         moment(event.date).format("DD.MM.YYYY") === props.currentDate ? (
-          <p key={event.time}>
-            Title: {event.title} <br />
-            Description: {event.description} <br />
-            Time: {event.time} <br />
-          </p>
+          <div>
+            <p key={event.id}>
+              Title: {event.title} <br />
+              Description: {event.description} <br />
+              Time: {event.time} <br />
+              <button onClick={props.removeEvent} value={event.id}>
+                delete
+              </button>
+            </p>
+          </div>
         ) : null
       )}
     </div>
   );
 };
 
+// This component handles creating, modifying, deleting and displaying events
 const Event = (props) => {
   const [events, setEvents] = useState([]);
   const [newTitle, setNewTitle] = useState("");
@@ -54,17 +63,19 @@ const Event = (props) => {
     setNewTime(event.target.value);
   };
 
+  // React effects and hooks are used to fetch data with axios from the JSON-server
   const hook = () => {
     EventService.getAll().then((response) => {
       setEvents(response.data);
       if (response.status === 200) {
-        console.log("get successful");
+        // console.log("get successful");
       }
     });
   };
 
   useEffect(hook, []);
 
+  // This function handles adding a new event and pushing it to the JSON-server
   const addEvent = (event) => {
     event.preventDefault();
     const EventObject = {
@@ -82,12 +93,19 @@ const Event = (props) => {
         console.log("post successful");
       })
       .catch((error) => {
-        setErrorMessage(`Error`);
+        setErrorMessage(error);
         setTimeout(() => {
           setErrorMessage(null);
         }, 5000);
       });
   };
+  // This function handles removing an existing event from the json-server
+  const removeEvent = (event) => {
+    const id = event.target.value;
+    EventService.remove(id).then(console.log("delete successful"));
+  };
+
+  // Here we render everything that would be displayed inside the modal that pops up when a date is clicked in the calendar
   return (
     <div
       id="event"
@@ -95,7 +113,11 @@ const Event = (props) => {
         e?.stopPropagation();
       }}
     >
-      <EventList events={events} currentDate={props.date} />
+      <EventList
+        events={events}
+        currentDate={props.date}
+        removeEvent={removeEvent}
+      />
       <EventForm
         title={newTitle}
         description={newEvent}
